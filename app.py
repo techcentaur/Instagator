923d352ca053ad8084b38a3e7665cb9acc049261
Ankit Solanki <ankit03june@gmail.com>
2018-05-23 15:48:44 +0530

    ADD: new functions for scraping user data in json format

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-import argparse
-import requests
 import json
 import pprint
+import requests
+import argparse
 
 
 class Instagator:
     
     def __init__(self, **kwargs):
-        default_attr = dict(username='', user_login='', pass_login='')
+        default_attr = dict(username='')
 
         allowed_attr = list(default_attr)
         default_attr.update(kwargs)
@@ -16,56 +16,53 @@ class Instagator:
             if key in allowed_attr:
                 self.__dict__[key] = default_attr.get(key)
 
-        self.session = requests.Session()
-        self.login()
+    def userpage_scraper(self, usernamelist):
+        self.base_url = 'https://www.instagram.com/'
 
-
-    def login(self):
-        base_url = 'https://www.instagram.com/'
-        login_url = base_url + 'accounts/login/ajax/'
-
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'
-        self.session.headers = {'user-agent' : user_agent}
-        self.session.headers.update({'Referer': base_url})
-
-        req = self.session.get(base_url)
-
-        self.session.headers.update({'X-CSRFToken': req.cookies['csrftoken']})
-        login_data = {'username': self.user_login, 'password': self.pass_login}
-
-        login = self.session.post(login_url, data=login_data, allow_redirects=True)
-        self.session.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
-
-        cookies = login.cookies
-        login_text = json.loads(login.text)
-
-        # print(json.dumps(login_text))
-
-        resp = self.session.get(base_url + self.username)
-        resp = resp.text
-        if resp is not None and '_sharedData' in resp:
-            sd = resp.split("window._sharedData = ")[1].split(";</script>")[0]
+        for user in usernamelist:
+            response = requests.get(self.base_url + user)
+            raw = response.text
             
-            data = (json.loads(sd))
+            if raw is not None and '_sharedData' in raw:
+                jsondata = raw.split("window._sharedData = ")[1].split(";</script>")[0]
+                
+                datadict = (json.loads(jsondata))
 
-            # print(data['config']['viewer']['biography'])
-            print(data['entry_data']['ProfilePage'][0]['graphql']['user']['biography'])
-            print(data['entry_data']['ProfilePage'][0]['graphql']['user']['full_name'])
-            print(data['entry_data']['ProfilePage'][0]['graphql']['user']['profile_pic_url_hd'])
-            print(data['entry_data']['ProfilePage'][0]['graphql']['user']['username'])
+        return(datadict)
+
+    def rootuser_info(self, datadict):
+
+        dict1 = datadict['entry_data']['ProfilePage'][0]['graphql']['user']
+
+        userdict = {}
+        keylist = ['username', 'full_name', 'id', 'is_private', 'external_url', 'profile_pic_url', 'profile_pic_url_hd', 'edge_follow', 'edge_followed_by']
+
+        for key in keylist:
+            userdict[key] = dict1[key]
+
+        print(userdict)
+
+
+    def images_url(self, datadict):
+        pass
+
+    @staticmethod
+    def pretty_print(datadict):
+
+        with open('jsondata.json', 'w') as outfile:
+            outfile.write(json.dumps(datadict, sort_keys=True, indent=4))
+
+        outfile.close()
 
 
 def main():
     parser = argparse.ArgumentParser(description='Instagator : Investigate people on Instagram')
+    
     parser.add_argument('username', help='User to be searched', default=None)
-    parser.add_argument('-u', '--user-login', '--user_login', help='Instagram Username', default=None, required=True)
-    parser.add_argument('-p', '--pass-login', '--pass_login', help='Instagram Password', default=None, required=True)
-
     args = parser.parse_args()
 
     insta = Instagator(**vars(args))
-    # var() method takes only one parameter. It takes an object as parameter and returns the __dict__ module
-
+    insta.userpage_scraper([args.username])
 
 if __name__=="__main__":
     main()
