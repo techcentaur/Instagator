effed777f22f518f5a8a6e0650db83233a9d45f8
Ankit Solanki <ankit03june@gmail.com>
2018-05-23 20:38:48 +0530

    NEW: function for scraping all posts information and getting it in dict

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 import pprint
 import requests
 import argparse
-
+from collections import OrderedDict
 
 class Instagator:
     
@@ -16,43 +16,81 @@ class Instagator:
             if key in allowed_attr:
                 self.__dict__[key] = default_attr.get(key)
 
-    def userpage_scraper(self, usernamelist):
+    def userpage_scraper(self, user):
+        """scrapes user json data from instagram response"""
+
         self.base_url = 'https://www.instagram.com/'
 
-        for user in usernamelist:
-            response = requests.get(self.base_url + user)
-            raw = response.text
+        response = requests.get(self.base_url + user)
+        raw = response.text
+        
+        if raw is not None and '_sharedData' in raw:
+            jsondata = raw.split("window._sharedData = ")[1].split(";</script>")[0]
             
-            if raw is not None and '_sharedData' in raw:
-                jsondata = raw.split("window._sharedData = ")[1].split(";</script>")[0]
-                
-                datadict = (json.loads(jsondata))
+            datadict = (json.loads(jsondata))
 
-        return(datadict)
+        return (datadict)
 
     def rootuser_info(self, datadict):
+        """returns user basic information in a dictionary"""
 
+        dict1 = OrderedDict()
         dict1 = datadict['entry_data']['ProfilePage'][0]['graphql']['user']
 
-        userdict = {}
-        keylist = ['username', 'full_name', 'id', 'is_private', 'external_url', 'profile_pic_url', 'profile_pic_url_hd', 'edge_follow', 'edge_followed_by']
+        userdict = OrderedDict()
+        keylist = ['id', 'username', 'full_name', 'edge_follow', 'edge_followed_by', 'is_private', 'external_url', 'profile_pic_url_hd']
 
         for key in keylist:
-            userdict[key] = dict1[key]
+            if key is 'edge_follow':
+                userdict['following'] = dict1[key]
+            elif key is 'edge_followed_by':
+                userdict['followers'] = dict1[key]
+            else:
+                userdict[key] = dict1[key]
 
-        print(userdict)
+        userdict['platform'] = datadict['platform']
+
+        return (json.dumps(userdict, indent=4))
 
 
-    def images_url(self, datadict):
-        pass
+    def user_images_url(self, datadict):
+        """returns all posts information in dictionary with keys as is_video, url, and caption"""
+
+        dict1 = datadict['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']
+        no_of_posts = dict1['count']
+
+        posts = dict1['edges']
+
+        posts_info = OrderedDict()
+        for count, post in enumerate(posts):
+            tempdict = OrderedDict()
+
+            tempdict['url'] = "https://www.instagram.com/p/" + post['node']['shortcode']
+            tempdict['is_video'] = post['node']['is_video']
+            tempdict['caption'] = post['node']['edge_media_to_caption']['edges'][0]['node']
+
+
+            posts_info[count] = tempdict
+
+        return (json.dumps(posts_info, indent=4))
 
     @staticmethod
-    def pretty_print(datadict):
+    def pretty_print(datadict, print_or_save):
 
-        with open('jsondata.json', 'w') as outfile:
-            outfile.write(json.dumps(datadict, sort_keys=True, indent=4))
+        if print_or_save:
+            json.dumps(datadict, sort_keys=True, indent=4)
+        else:
+            with open('jsondata.json', 'w') as outfile:
+                outfile.write(json.dumps(datadict, sort_keys=True, indent=4))
+            outfile.close()
 
-        outfile.close()
+        return 0
+
+    def maincall(self, usernamelist):
+        dict1 = self.userpage_scraper(usernamelist)
+        self.rootuser_info(dict1)
+        self.user_images_url(dict1)
+
 
 
 def main():
@@ -62,7 +100,7 @@ def main():
     args = parser.parse_args()
 
     insta = Instagator(**vars(args))
-    insta.userpage_scraper([args.username])
+    insta.maincall(args.username)
 
 if __name__=="__main__":
     main()
